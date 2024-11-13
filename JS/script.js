f120820e6b4badce6c8d73332c6c4d28d58131f9
SimoneZampieri <simonezampieri106@gmail.com>
2024-11-13 14:53:07 +0100

    esercizio completato

--- JS/script.js
+++ JS/script.js
@@ -10,13 +10,28 @@
 
 const prezzoKm = 0.21;
 const scontoMinori = 20;
-const scontOver = 40;
+const scontoOver = 40;
 
 //dati da sapere
-const kmTotali = prompt(Quanti KM devi percorrere?);
-const etaPasseggero = prompt(Quanti anni Hai?);
+
+const kmTotali = prompt('Quanti KM devi percorrere?');
+const etaPasseggero = prompt('Quanti anni Hai?');
 
 //svolgimento
 let prezzoBiglietto;
 let calcoloPrezzo;
-let prezzoTotKm = prezzoKm * kmTotali;
+let prezzoTotKm = kmTotali * prezzoKm;
+
+if (etaPasseggero < 18){
+    calcoloPrezzo = (prezzoTotKm / 100) * (100 - scontoMinori)
+} else if (etaPasseggero > 65){
+    calcoloPrezzo = (prezzoTotKm / 100) * (100 - scontoOver)
+} else {
+    calcoloPrezzo = prezzoTotKm
+}
+
+//output
+
+prezzoBiglietto = calcoloPrezzo.toFixed(2);
+console.log( ` Il prezzo totale è: ${prezzoBiglietto}€ ` );
+alert ( ` Il prezzo totale è: ${prezzoBiglietto}€ ` );
